721427d514442c0b0eeb0f42a76105bc0eea61f7
jenkins <builds@apache.org>
2018-09-14 19:26:10 +0000

    PHP SDK documentation based on apache/predictionio-sdk-php#de58c85bfcf07c82d1f57c33221fa7dae998a34c

---
+++ sdk/php/api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","predictionio\\BaseClient"],["c","predictionio\\EngineClient"],["c","predictionio\\EventClient"],["c","predictionio\\Exporter"],["c","predictionio\\FileExporter"],["c","predictionio\\PredictionIOAPIError"]];
